dcc4999e1d83746823a1e5aed0371f5c6511d237
Rene Mandler <r.mandler@overlap.at>
2024-05-17 10:08:11 +0200

    deploy

---
+++ _next/static/chunks/233.33a0b248a76bf949.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[233],{4233:function(l,s,e){e.r(s);var c=e(2322);s.default=()=>(0,c.jsx)("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",children:(0,c.jsx)("path",{fill:"currentColor",d:"M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3l7 3V5c0-1.1-.9-2-2-2"})})}}]);
