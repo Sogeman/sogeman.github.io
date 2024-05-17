dfb4d0089355cb70c34c4161fa088c43327f9d55
Rene Mandler <32226747+Sogeman@users.noreply.github.com>
2024-05-17 09:54:43 +0200

    Add files via upload

---
+++ _next/static/chunks/899.048f1a65002162e0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[899],{3899:function(s,e,l){l.r(e);var r=l(2322);e.default=()=>(0,r.jsx)("svg",{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 -960 960 960",children:(0,r.jsx)("path",{d:"m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224z",fill:"currentColor"})})}}]);
---
+++ icons/icon-fav-fill.tsx
@@ -0,0 +1,10 @@
+const IconFavFill = (): JSX.Element => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+        <path
+            d="m480-120-58-52q-101-91-167-157T150-447.5 95.5-544 80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5 705-329 538-172z"
+            fill="currentColor"
+        />
+    </svg>
+);
+
+export default IconFavFill;
